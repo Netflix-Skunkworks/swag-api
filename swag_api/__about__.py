0a899b2bde75e2d7d9fb075b1c5aef2458371409
Will Bengtson <wbengtson@netflix.com>
2018-05-25 18:30:28 -0700

    version bump

--- swag_api/__about__.py
+++ swag_api/__about__.py
@@ -10,7 +10,7 @@ __title__ = "swag-api"
 __summary__ = ("API service for SWAG data")
 __uri__ = "https://github.com/Netflix-Skunkworks/swag-api"
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __author__ = "The SWAG developers"
 __email__ = "security@netflix.com"
